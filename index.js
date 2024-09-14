a32992c5bb68d30e817d6b13d63402276d8ab39b
Dhickytriaji <dhickytriajisaputro86@gmail.com>
2024-09-14 16:21:18 +0700

    update terbaru

---
+++ index.js
@@ -0,0 +1,20 @@
+var cars = "avanza, mobilio, ayla, brio, jazz"
+console.log(cars)
+// document.writeln(cars)
+
+
+var gretting = "Hello Words"
+console.log(cars)
+// document.writeln(gretting)
+
+
+var dataString = "ini string"
+var dataNumber = 12345
+var dataBooelan = true //false
+var a = 20
+var b = 30
+result = a*b
+console.log(dataString)
+console.log(dataNumber)
+console.log(dataBooelan)
+console.log(result)
