1f7ef85c2c5789da23d1856b3cddb5dbaf1c9e01
Dhickytriaji <dhickytriajisaputro86@gmail.com>
2024-09-15 12:01:20 +0700

    first commit

--- index.js
+++ index.js
@@ -18,3 +18,99 @@ console.log(dataString)
 console.log(dataNumber)
 console.log(dataBooelan)
 console.log(result)
+
+// operation aritmatika
+//var dataString = "ini bukan string"
+//console.log (dataString)
+
+//const dataConts = "hello"
+//const dataConts = "world"
+//console.log(dataConst)
+
+//operation aritmetika
+const resultTambah = 3 + 5
+console.log(resultTambah)
+
+const resultKurang = 3 - 5
+console.log(resultKurang)
+
+const resultBagi = 3 / 5
+console.log(resultBagi)
+
+const resultSisaBagi = 10 % 5
+console.log(resultSisaBagi)
+
+//operator perbandingan
+const resultTipeData = 5 == '5' //Berfungsi untuk mengecek tipe data
+//const resultTipeData = 5 == '5' //Tidak ngecek tipe data
+console.log (resultTipeData)
+
+const resultLebihDari = 5 > 3
+console.log(resultLebihDari)
+
+const resultKurangDari = 5 < 3
+console.log(resultKurangDari)
+
+//operator logika
+const nilaiUjian = 65
+const nilaiAbsen = 76
+
+const lulusUjian = nilaiUjian >= 75
+const lulusAbsen = nilaiAbsen >= 75
+
+const lulusSempurna = lulusUjian && lulusAbsen
+const lulusSaja = lulusUjian || lulusAbsen
+
+console.log('Lulus Sempurna', lulusSempurna)
+console.log('Lulus Saja', lulusSaja)
+
+//contitional
+// if (nilaiUjian >= 75) {
+//     console.log('LULUS')
+// } else {
+//     console.log('TIDAK LULUS')
+// }
+
+// if (nilaiAbsen >= 75) {
+//     console.log('LULUS')
+// } else {
+//     console.log('TIDAK LULUS')
+// }
+
+if (nilaiUjian >= 75 && nilaiAbsen < 75) {
+    console.log("LULUS SEMPURNA")
+} else if (nilaiUjian < 75 && nilaiAbsen>=75){
+    console.log("LULUS SAJA")
+} else if (nilaiUjian >= 75 && nilaiAbsen < 75){
+    console.log("LULUS SAJA")
+} else {
+    console.log("TIDAK LULUS")
+}
+
+//TERNERY OPERATOR
+const kelulusanSiswa = nilaiUjian >= 75 && nilaiAbsen >= 75 ? 'LULUS SEMPURNA'  : (nilaiUjian <75 && nilaiAbsen >= 75) || nilaiUjian >= 75 && nilaiAbsen < 75 ? 'LULUS' : 'TIDAK LULUS'
+console.log('HASIL KELULUSAN', kelulusanSiswa)
+
+
+//ARRAY
+const mobil = [
+    'ayla',
+    'agya',
+    'calya',
+    'sigra',
+    'jazz',
+    'mobilio',
+    'BRV'
+]
+console.log(mobil)
+console.table(mobil)
+
+    //get data index n 
+    console.log('Data Index ke 4 =', mobil[4] )
+
+    //Find Length/Jumlah data
+    console.log(mobil.length)
+
+    //ARRAY METHOD
+    mobil.push('kijang')
+    
