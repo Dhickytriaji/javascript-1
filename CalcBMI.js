c91cfe1c5741c4ceb30fdef10b8a6aef3f2e2beb
Dhickytriaji <dhickytriajisaputro86@gmail.com>
2024-09-22 10:36:16 +0700

    push js

--- CalcBMI.js
+++ CalcBMI.js
@@ -15,21 +15,30 @@ function Calcbmi(){
 
 
     //indicator category
-    if (CalcbmiCount < 0 ){
-        indocator = "Result not valid"
-     } else if(CalcbmiCount > 0 && CalcbmiCount < 18.05){
-        indocator =  "Under Weight"
-     } else if(CalcbmiCount >=18.05 && CalcbmiCount <= 25.09){
-        indocator =  "Healty Weight"
-     } else if(CalcbmiCount >=25.09 && CalcbmiCount <=29,9){
-        indocator =  "Over Weight"
-     } else if(CalcbmiCount >=29,9 && CalcbmiCount <= 50){
-        indocator =  "obesity"
-     } else if(CalcbmiCount >=35 && CalcbmiCount <= 39){
-    indocator =  "severe obesity"
-     }
-     console.log(indocator)
-    
 
+   const indicatorColor = document.getElementById('resultBox')
+
+    if (CalcbmiCount < 0 ){
+        indicator = "Result not valid"
+        indicatorColor.classList.toggle('NotValidColor' , bmiCalcCount)
+     } else if(CalcbmiCount > 0 && CalcbmiCount < 18.05){
+        indicator =  "UnderWeight"
+        indicatorColor.classList.toggle('underWeight' bmiCalcCount)
+     } else if(CalcbmiCount >=18.05 && CalcbmiCount <= 25.09){
+        indicator =  "HealtyWeight"
+        indicatorColor.classList.toggle('healtyWeightColor' bmiCalcCount)
+     } else if(CalcbmiCount >=25.09 && CalcbmiCount <=29,9){
+        indicator =  "OverWeight"
+        indicatorColor.classList.toggle('overWeightColor' bmiCalcCount)
+     } else if(CalcbmiCount >=29,9 && CalcbmiCount <= 50){
+        indicator =  "obesity"
+        indicatorColor.classList.toggle('obesityColor' bmiCalcCount)
+     } else if(CalcbmiCount >=35 && CalcbmiCount <= 39){
+    indicator =  "severeObesity"
+    indicatorColor.classList.toggle('severeObesityColor' bmiCalcCount)
+     }
+     //console.log(indicator)
+    
+     document.getElementById('resultCategory').innerHTML = indicator
 
 }
